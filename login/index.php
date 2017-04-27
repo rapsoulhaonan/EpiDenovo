93b180a4c583fd80fd0cb21b5fb5e423d02b3d3d
Haonan Yang <hyang7@emich.edu>
2017-04-27 14:59:09 -0400

    Add files via upload

---
+++ login/index.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RCRDB Bioinformatics</title>
+</head>
+	<meta name="description" content="Academic and higher education website design. Websites for educational institutions, academic departments, research labs, institutes, and non-profits." /><meta name="keywords" content="" />                         <!-- keyword and description from CMS -->
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+
+<!-- to replace the favicon change the href below to point to one of your own images -->
+<link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico" />
+
+<!-- enable bootstrap responsive  -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- bootstrap  -->
+<link rel="stylesheet" href="/bootstrap.min.css">
+
+<!-- customization  -->
+<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic' rel='stylesheet' type='text/css'>
+<link href="/style.css" rel="stylesheet">
+<link href="/submit-bar.css" rel="stylesheet">
+
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements and IE8 meida query support -->
+<!--[if lt IE 9]>
+  <script src="https://assets-production-webvanta-com.s3-us-west-2.amazonaws.com/000000/49/06/original/js/ie.js"></script>
+<![endif]-->
+
+
+    <link rel="stylesheet" href="/js/flexslider/flexslider.css" />
+
+<style type="text/css">
+  .b-top h2 {
+    font-weight:normal;
+    font-size:32px;
+    line-height: 38px;
+  }
+  .content-area {
+    background:#fff;
+    margin-bottom:20px;
+    border-radius: 6px;
+  } 
+  .logos_container {
+    height:60px;
+    margin: 0 1% 0 2%;
+  }
+  .logos_container .content-box ul {
+    padding-left:0px;
+  }
+  #customer-logos li {
+    padding-right: 30px;
+    float: left;
+    display: block;
+  }
+  #customer-logos li:last-child {
+    padding-right:0px;
+  }
+  */
+</style> <!-- page-specific CSS -->
+    <script type="text/javascript">
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-3052557-2']);
+_gaq.push(['_trackPageview']);
+
+(function() {    
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
+})();
+
+</script>
+
+<script type="text/javascript" src="https://webvanta.infusionsoft.com/app/webTracking/getTrackingCode?trackingId=f9b44772ae6a0f9da177de80dab0f684"></script>
+
+<script type="text/javascript">
+  (function() {
+    window._pa = window._pa || {};
+    // _pa.orderId = "myOrderId"; // OPTIONAL: attach unique conversion identifier to conversions
+    // _pa.revenue = "19.99"; // OPTIONAL: attach dynamic purchase values to conversions
+    // _pa.productId = "myProductId"; // OPTIONAL: Include product ID for use with dynamic ads
+    var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
+    pa.src = ('https:' == document.location.protocol ? 'https:' : 'http:') + "//tag.marinsm.com/serve/55e11699087a52bc1e000011.js";
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(pa, s);
+  })();
+</script>
+
+<body class="home">
+	<div class="container">
+		<nav class="navbar" role="navigation">
+		  <div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+			  <span class="toggle-text"><a href="home.html">Main Menu</a></span>
+			  <span class="toggle-bars">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			  </span>
+			</button>
+		  </div>
+	</div>
+	<div class="container">
+		<nav class="navbar" role="navigation">
+		  <div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+			  <span class="toggle-text"><a href="logout.php">Logout</a></span>
+			  <span class="toggle-bars">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			  </span>
+			</button>
+		  </div>
+	</div>
+	<div class="collapse navbar-collapse navbar-ex1-collapse">
+      <ul class="nav navbar-nav">
+		
+		<!-- Home Bar with drop down -->
+		<li class="dropdown  ">
+              <a href='#' data-toggle="dropdown" class="dropdown-toggle">RCRDB BioSearching Engine Home<b class="caret"></b></a>
+              <div class="dropdown-menu-container websites-for-higher-education-container">
+                <ul class="dropdown-menu websites-for-higher-education-sub">
+                    <li class=" ">
+                        <a href='/academic-department-websites'>About RCRDB</a>
+                    </li>
+                    <li class=" ">
+                        <a href='/research-project-websites'>Research and Analysis Demostration</a>
+                    </li>      
+                    <li class=" ">
+                        <a href='/work/academic-website-examples'>News and Update</a>
+                    </li>         
+				    
+					
+		<li class="dropdown  ">
+			<a href='#' data-toggle="dropdown" class="dropdown-toggle">Search<b class="caret"></b></a>
+				<div class="dropdown-menu-container websites-for-mission--based-organizations-container">
+					<ul class="dropdown-menu websites-for-mission--based-organizations-sub">
+                  
+                    
+                    <li class=" ">
+                        <a href='/websites-for-mission-based-organizations'>Information Portals</a>
+                    </li>
+                 
+                    
+                    <li class=" ">
+                        <a href='/work/mission-based-website-examples'>Example Websites</a>
+                    </li>
+                 
+
+		<li class="dropdown  ">
+            <a href='#' data-toggle="dropdown" class="dropdown-toggle">Stats<b class="caret"></b></a>
+              <div class="dropdown-menu-container about-us-container">
+                <ul class="dropdown-menu about-us-sub">
+                  
+                    
+                    <li class=" ">
+                        <a href='/about'>Collected Public Data Sets</a>
+                    </li>
+                 
+                    
+                    <li class=" ">
+                        <a href='/team'>Distributed with regulated genes</a>
+                    </li>
+                 
+                    
+                    <li class=" ">
+                        <a href='/process'>Different typse of RNA-Seq</a>
+                    </li>
+                 
+                    
+                    <li class=" ">
+                        <a href='/technology'>Differential expression of WT VS KD/KO samples</a>
+                    </li>
+                 
+                    
+                    <li class=" ">
+                        <a href='/testimonials'>ChIP-Seq data sets of transcriptional factors or histone modifications</a>
+                    </li>
+                 
+				 
+		<li class="dropdown  ">
+            <a href='#' data-toggle="dropdown" class="dropdown-toggle">Documentation<b class="caret"></b></a>
+              <div class="dropdown-menu-container learn-more-container">
+                <ul class="dropdown-menu learn-more-sub">
+                  
+                    
+                    <li class=" ">
+                        <a href='/resources/webinars'>About construction of RCRDB</a>
+                    </li>
+                 
+                    
+                    <li class=" ">
+                        <a href='/resources/blog'>Workflow of the construction of RCRDB</a>
+                    </li>
+					
+					<li class=" ">
+                        <a href='/resources/blog'>Rhythmic genes identify from different types of RNA-Seq</a>
+                    </li>
+					
+					<li class=" ">
+                        <a href='/resources/blog'>Distribution of rgulated genes in different regulatory class</a>
+                    </li>
+					
+					<li class=" ">
+                        <a href='/resources/blog'>How to use RCRDB?</a>
+                    </li>
+                 
+				 
+				    <li class="dropdown  ">
+              <a href='#' data-toggle="dropdown" class="dropdown-toggle">Download<b class="caret"></b></a>
+              <div class="dropdown-menu-container contact-container">
+                <ul class="dropdown-menu contact-sub">
+                  
+                    
+                    <li class=" ">
+                        <a href='/contact/schedule-consultation'>Collected data table</a>
+                    </li>
+                 
+                    
+                    <li class=" ">
+                        <a href='http://helpdesk.webvanta.com/hc/en-us/requests/new'>Regulatory network</a>
+                    </li>
+                 
+                    
+                    <li class=" ">
+                        <a href='/contact'>Regulatory information of each class </a>
+                    </li>
+                 
+				    
+					<li class="dropdown  ">
+              <a href='#' data-toggle="dropdown" class="dropdown-toggle">Links<b class="caret"></b></a>
+              <div class="dropdown-menu-container contact-container">
+                <ul class="dropdown-menu contact-sub">
+                  
+					
+					
+					<li class="dropdown  ">
+              <a href='#' data-toggle="dropdown" class="dropdown-toggle">Contact<b class="caret"></b></a>
+              <div class="dropdown-menu-container contact-container">
+                <ul class="dropdown-menu contact-sub">
+				
+					<li class="dropdown  ">
+              <a href='#' data-toggle="dropdown" class="dropdown-toggle">News<b class="caret"></b></a>
+              <div class="dropdown-menu-container contact-container">
+                <ul class="dropdown-menu contact-sub">
+                  
+                 
+                </ul>   
+              </div> 
+           </li>
+
+				 
+				 
+                </ul>   
+              </div> 
+           </li>
+
+				 
+				 
+				 
+                </ul>   
+              </div> 
+           </li>
+
+				 
+				 
+				 
+                </ul>   
+              </div> 
+           </li>
+
+                </ul>   
+              </div> 
+           </li>		   
+</body>
+</html>
